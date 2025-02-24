34bc9b5e490f45677e1b2be764af93831cd35393
Elizaveta Korotkova <kavasima1994@icloud.com>
2025-02-24 08:06:11 +0200

    add courier delete after create

--- conftest.py
+++ conftest.py
@@ -1,3 +1,4 @@
 LOGIN_URL = "https://qa-scooter.praktikum-services.ru/api/v1/courier/login"
 ORDERS_URL = "https://qa-scooter.praktikum-services.ru/api/v1/orders"
 COURIER_URL = "https://qa-scooter.praktikum-services.ru/api/v1/courier"
+COURIER_DELETE = "https://qa-scooter.praktikum-services.ru/api/v1/api/v1/courier/:id"
--- tests/test_create_courier.py
+++ tests/test_create_courier.py
@@ -2,6 +2,8 @@ import requests
 from utils import generate_courier_data
 import allure
 from conftest import COURIER_URL
+from conftest import LOGIN_URL
+from conftest import COURIER_DELETE
 
 
 class TestCourierCreate:
@@ -19,3 +21,25 @@ class TestCourierCreate:
         with allure.step("Проверяем код ответа и текст в ответе"):
             assert response.status_code == 201
             assert response_data == {'ok': True}, f"Ошибка: ожидали {{'ok': True}}, но получили {response_data}"
+
+        login_payload = {
+            "login": payload["login"],
+            "password": payload["password"]
+        }
+        with allure.step("Логинимся и получаем ID курьера"):
+            login_response = requests.post(f"{COURIER_URL}/login", json=login_payload)
+            login_data = login_response.json()
+
+            assert login_response.status_code == 200, f"Ошибка входа: {login_data}"
+            assert "id" in login_data, f"В ответе отсутствует 'id': {login_data}"
+            courier_id = login_data["id"]
+
+        # Удаляем курьера
+        with allure.step("Удаляем курьера"):
+            delete_response = requests.delete(f"{COURIER_URL}/{courier_id}")
+            delete_data = delete_response.json()
+
+            assert delete_response.status_code == 200, f"Ошибка удаления: {delete_data}"
+            assert delete_data == {'ok': True}, f"Ошибка удаления: {delete_data}"
+
+            print(f"Курьер {courier_id} удален")
